fa9ab47570588961e347629d9869daeed0efbfe6
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-10-17 20:28:11 +0500

    adding array list

--- src/Child.js
+++ src/Child.js
@@ -1,6 +1,13 @@
 import React from 'react'; 
 
 function Child() {
+
+    let transaction = [
+        {amount: 500, desc: "cash"},
+        {amount: -200, desc: "book"},
+        {amount: -100, desc: "camera"},
+
+    ]
   return (
     <div className="container">
     <h1 className="text_cenetr">Expence Tracker</h1>
@@ -12,15 +19,35 @@ function Child() {
     </div>
     <h3>History</h3>
     <hr/>
+
+    <ul className="transaction-list">
+<li>
+    <span>Cash</span>
+    <span>+500</span>
+
+</li>
+
+<li>
+    <span>Cash</span>
+    <span>+500</span>
+
+</li>
+
+<li>
+    <span>Cash</span>
+    <span>+500</span>
+
+</li>
+    </ul>
     <h3>Add new transaction</h3>
     <hr/>
 
     <form className="transaction-form">
         <label>Enetr Description <br/>
-        <input type="text"/>
+        <input type="text" required/>
         </label><br/>
         <label>Enter Amount <br/>
-        <input type="number"/>
+        <input type="number" required/>
         </label><br/>
 
         <input type="submit" value="Add Transaction"/>
