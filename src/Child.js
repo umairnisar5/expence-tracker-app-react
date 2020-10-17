4acdadbff54dea28de9d9dc85b9fc2a79c856471
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-10-17 20:09:16 +0500

    adding css / form

--- src/Child.js
+++ src/Child.js
@@ -2,9 +2,31 @@ import React from 'react';
 
 function Child() {
   return (
-    <div>
-    <h1>this is Child</h1>
+    <div className="container">
+    <h1 className="text_cenetr">Expence Tracker</h1>
+    <h3>your Balnce <br/> $260</h3>
+
+    <div className="expence_container">
+    <h3>Income <br/> $260</h3>
+    <h3>Expence <br/> $200</h3>
     </div>
+    <h3>History</h3>
+    <hr/>
+    <h3>Add new transaction</h3>
+    <hr/>
+
+    <form className="transaction-form">
+        <label>Enetr Description <br/>
+        <input type="text"/>
+        </label><br/>
+        <label>Enter Amount <br/>
+        <input type="number"/>
+        </label><br/>
+
+        <input type="submit" value="Add Transaction"/>
+    </form>
+    </div>
+
   );
 }
 
